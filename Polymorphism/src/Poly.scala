194f924f58607f00dde1ef142b0cc35f4b436634
Matias Berretta <matiasberretta93@gmail.com>
2017-12-28 20:31:10 -0200

    Add files via upload

---
+++ Polymorphism/src/Poly.scala
@@ -0,0 +1,41 @@
+/**
+  * Created by MatiasBerretta on 12/28/17.
+  */
+object Poly {
+
+  val listStrings = List("Benito","Veronica", "Matias", "Juanbi", "Catu", "Justi", "Balti")
+  val listIntegers = List(55, 50, 25, 23, 21, 16, 15)
+
+  def hardSearchKey(a: List[String], key: String): Int = {
+    def loop(n: Int): Int ={
+      if (n > a.length) -1
+      else if (a(n) == key) n+1 // +1 in order to account for zero-index
+      else loop(n+1)
+    }
+    loop(0)
+  }
+
+  def abstractSearchKey[A](a: List[A], p: A): Int = {
+    def loop(n: Int): Int ={
+      if (n > a.length) -1
+      else if (a(n) == p) n+1 // If the function p matches the current element, we've found a match
+      else loop(n+1)
+    }
+    loop(0)
+  }
+
+  private def formatHardSearchKey(x: String) = {
+    val msg = "%s was found at position #%d of the list"
+    msg.format(x, hardSearchKey(listStrings, x))
+  }
+
+  private def formatAbstractSearchKey(x: Int) = {
+    val msg = "%d was found at position #%d of the list"
+    msg.format(x, abstractSearchKey[Int](listIntegers, x))
+  }
+
+  def main(args: Array[String]): Unit = { //Unit serves the same purpose as void in languages like Java or C
+    println(formatHardSearchKey("Matias"))
+    println(formatAbstractSearchKey(25))
+  }
+}
