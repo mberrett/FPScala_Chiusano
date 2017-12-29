f69292377a322155a41ae37d948e4843c80da295
Matias Berretta <matiasberretta93@gmail.com>
2017-12-29 13:16:19 -0200

    Update Poly.scala

--- Polymorphism/src/Poly.scala
+++ Polymorphism/src/Poly.scala
@@ -1,12 +1,9 @@
-/**
-  * Created by MatiasBerretta on 12/28/17.
-  */
 object Poly {
 
   val listStrings = List("Benito","Veronica", "Matias", "Juanbi", "Catu", "Justi", "Balti")
   val listIntegers = List(55, 50, 25, 23, 21, 16, 15)
 
-  def hardSearchKey(a: List[String], key: String): Int = {
+  def hardSearchIndex(a: List[String], key: String): Int = {
     def loop(n: Int): Int ={
       if (n > a.length) -1
       else if (a(n) == key) n+1 // +1 in order to account for zero-index
@@ -15,7 +12,7 @@ object Poly {
     loop(0)
   }
 
-  def abstractSearchKey[A](a: List[A], p: A => Boolean): Int = {
+  def abstractSearchIndex[A](a: List[A], p: A => Boolean): Int = {
     def loop(n: Int): Int ={
       if (n > a.length) -1
       else if (p(a(n))) n+1 // If the function p matches the current element, we've found a match
@@ -24,18 +21,18 @@ object Poly {
     loop(0)
   }
 
-  private def formatHardSearchKey(x: String) = {
+  private def formatHardSearchIndex(x: String) = {
     val msg = "%s was found at position #%d of the list"
-    msg.format(x, hardSearchKey(listStrings, x))
+    msg.format(x, hardSearchIndex(listStrings, x))
   }
 
-  private def formatAbstractSearchKey(x: Int) = {
+  private def formatAbstractSearchIndex(x: Int) = {
     val msg = "%d was found at position #%d of the list"
-    msg.format(x, abstractSearchKey[Int](listIntegers, y => y == x)) // lambda function to satisfy type (Int) => Boolean
+    msg.format(x, abstractSearchIndex[Int](listIntegers, y => y == x)) // lambda function to satisfy type (Int) => Boolean
   }
 
   def main(args: Array[String]): Unit = { //Unit serves the same purpose as void in languages like Java or C
-    println(formatHardSearchKey("Matias"))
-    println(formatAbstractSearchKey(25))
+    println(formatHardSearchIndex("Matias"))
+    println(formatAbstractSearchIndex(25))
   }
 }
